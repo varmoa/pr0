7d6fcd9814022efe88ac3f132c3b8468c8cbe541
Victor Armoa <47931189+varmoa@users.noreply.github.com>
2024-04-09 16:54:10 -0300

    Update pro1.ps1
    agregado todos los accesos

--- pro1.ps1
+++ pro1.ps1
@@ -77,6 +77,45 @@ function InstalarOffice365 {
     Read-Host "Presiona Enter para continuar..."
 }
 
+function InstalarAcceso($ruta, $scriptVBS = $null, $icono = $null, $accesoDirecto = $null) {
+    $rutaCompleta = Join-Path -Path "\\aest-repo1\paquetes" -ChildPath $ruta
+
+    if ($scriptVBS) {
+        & cscript.exe (Join-Path -Path $rutaCompleta -ChildPath $scriptVBS)
+    }
+
+    if ($icono) {
+        Copy-Item -Path (Join-Path -Path $rutaCompleta -ChildPath $icono) -Destination "C:\Windows\" -Force
+    }
+
+    if ($accesoDirecto) {
+        Copy-Item -Path (Join-Path -Path $rutaCompleta -ChildPath $accesoDirecto) -Destination "C:\Users\Public\Desktop\" -Force
+    }
+
+    Write-Host "###########"
+
+# SAP Concur-acceso
+InstalarAcceso -ruta "UPGRADE y MANTENIMIENTO\SAP_Concur-acceso" -scriptVBS "Install.vbs"
+
+# SMART-acceso
+InstalarAcceso -ruta "UPGRADE y MANTENIMIENTO\SMART-acceso" -scriptVBS "Install.vbs"
+
+# Acceso Directo Tapps (nuevo)
+InstalarAcceso -ruta "Acceso Directo\Acceso Directo Tapps (nuevo)" -icono "Tapps.ico" -accesoDirecto "Tapps.lnk"
+
+# Acceso Directo Itickets
+InstalarAcceso -ruta "UPGRADE y MANTENIMIENTO\Acceso Directo Itickets" -icono "itickets.ico" -accesoDirecto "itickets.lnk"
+
+# Acceso Directo Teco XP
+InstalarAcceso -ruta "Acceso Directo\Acceso Directo Teco XP" -icono "TecoXP.ico" -accesoDirecto "Teco XP.url"
+
+# Pausa al final del script
+Write-Host "Presione cualquier tecla para continuar..."
+$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+}
+
+
 # Función para mostrar el menú
 function MostrarMenu {
     do {
@@ -86,7 +125,7 @@ function MostrarMenu {
         Write-Host "1. Instala SAPLogon y crea conexiones"
         Write-Host "2. Instala GIRAFE"
         Write-Host "3. Instala OpenSmart"
-        Write-Host "4. Instala Condis"
+        Write-Host "4. Instala accesos"
         Write-Host "5. Instala Onefield"
         Write-Host "6. OFFICE 365"
         Write-Host "99. EXIT"
@@ -101,6 +140,7 @@ function MostrarMenu {
             '4' { InstalarCondis }
             '5' { InstalarOnefield }
             '6' { InstalarOffice365 }
+            '6' { InstalarAcceso }
             '99' { exit }
             default { Write-Host "Opción no válida. Por favor, selecciona una opción válida." }
         }
