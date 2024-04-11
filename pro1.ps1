6dfc59e48e4d8567fe3b6b8e7a093315e8e3bf9d
varmoa <argentrino@gmail.com>
2024-04-11 12:49:49 -0300

    funcion todos los accesos con copi ico e lnk

--- pro1.ps1
+++ pro1.ps1
@@ -80,21 +80,28 @@ function InstalarOffice365 {
 # Función para instalar accesos
 function InstalarAccesos {
     $url = "\\aest-repo1\paquetes\UPGRADE y MANTENIMIENTO\SAP_Concur-acceso"
-    #$url1 = "\\aest-repo1\paquetes\UPGRADE y MANTENIMIENTO\SMART-acceso"
+    $url1 = "\\aest-repo1\paquetes\UPGRADE y MANTENIMIENTO\SMART-acceso"
     $url2 = "\\aest-repo1\paquetes\Acceso Directo\Acceso Directo Tapps (nuevo)"
     $url3 = "\\aest-repo1\paquetes\UPGRADE y MANTENIMIENTO\Acceso Directo Itickets"
     $url4 = "\\aest-repo1\paquetes\UPGRADE y MANTENIMIENTO\SAP_Concur-acceso\x64"
+    $url5 = "\\aest-repo1\paquetes\UPGRADE y MANTENIMIENTO\SMART-acceso"
+    $url6 = "\\aest-repo1\paquetes\Acceso Directo\Acceso Directo Teco XP"
     $rut2 = "C:\Windows\"
     $rut3 = "C:\Users\Public\Desktop\"
     Write-Host "SAPConcur"
     Copy-Item "$url\SapConcur.ico" -Destination $rut2 -Force
     Copy-Item "$url4\SAP Concur.lnk" -Destination $rut3 -Force
+    Write-Host "Smart"
+    Copy-Item "$url1\Smart.ico" -Destination $rut2 -Force
+    Copy-Item "$url5\Smart.lnk" -Destination $rut3 -Force
     Write-Host "Tapps"
     Copy-Item "$url2\Tapps.ico" -Destination $rut2 -Force
     Copy-Item "$url2\Tapps.lnk" -Destination $rut3 -Force
     Write-Host "Itickets"
     Copy-Item "$url3\itickets.ico" -Destination $rut2 -Force
-    Copy-Item "$url2\itickets.lnk" -Destination $rut3 -Force
+    Copy-Item "$url3\itickets.lnk" -Destination $rut3 -Force
+    Copy-Item "$url6\TecoXP.ico" -Destination $rut2 -Force
+    Copy-Item "$url6\Teco XP.url" -Destination $rut3 -Force
     Read-Host "Presione cualquier tecla para continuar..."
     }  
 # function InstalarAccesos {
